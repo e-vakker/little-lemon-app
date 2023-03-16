a9887feed21e90ea802e8b2a9202a09f40f143e8
Jevgeni Vakker <bandem@icloud.com>
2023-03-16 22:35:09 +0100

    add an empty project

---
+++ LittleLemonApp/LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Jevgeni Vakker on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
