a1c31c2c202c45a48d3fa9f581a713d376f5b3ce
Jevgeni Vakker <bandem@icloud.com>
2023-03-16 22:37:31 +0100

    add an empty project

---
+++ LittleLemonApp/LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Jevgeni Vakker on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
