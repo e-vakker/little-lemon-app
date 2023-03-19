9c370ef7d960597f8ce8f774a309c12ae077683f
Jevgeni Vakker <bandem@icloud.com>
2023-03-19 22:15:20 +0100

    add a new screens with low code

--- LittleLemonApp/LittleLemonApp/Views/Onboarding.swift
+++ LittleLemonApp/LittleLemonApp/Views/Onboarding.swift
@@ -10,6 +10,7 @@ import SwiftUI
 let kFirstName = "first name key"
 let kLastName = "last name key"
 let kEmail = "e-mail key"
+let kIsLoggedIn = "kIsLoggedIn"
 
 struct Onboarding: View {
     @State var firstName = ""
@@ -22,61 +23,78 @@ struct Onboarding: View {
     @State var errorMessageShow = false
     @State var errorMessage = ""
     
+    @State var isLoggedIn = false
+    
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack {
-                Image("Logo")
-                    .padding(.bottom)
-                HeroSection()
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
                 VStack {
-                    Text("First name *")
-                        .onboardingTextStyle()
-                    TextField("First Name", text: $firstName)
-                    Text("Last name *")
-                        .onboardingTextStyle()
-                    TextField("Last Name", text: $lastName)
-                    Text("E-mail *")
-                        .onboardingTextStyle()
-                    TextField("E-mail", text: $email)
-                        .keyboardType(.emailAddress)
+                    Image("logo")
+                        .padding(.bottom)
+                    Menu()
+                    VStack {
+                        NavigationLink(destination: Home(), isActive: $isLoggedIn) {
+                            
+                        }
+                        Text("First name *")
+                            .onboardingTextStyle()
+                        TextField("First Name", text: $firstName)
+                        Text("Last name *")
+                            .onboardingTextStyle()
+                        TextField("Last Name", text: $lastName)
+                        Text("E-mail *")
+                            .onboardingTextStyle()
+                        TextField("E-mail", text: $email)
+                            .keyboardType(.emailAddress)
+                    }
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                    
+                    if errorMessageShow {
+                        withAnimation() {
+                            Text(errorMessage)
+                                .foregroundColor(.red)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.leading)
+                        }
+                    }
+                    
+                    Button("Register") {
+                        if validateUserInput(firstName: firstName, lastName: lastName, email: email) {
+                            UserDefaults.standard.set(firstName, forKey: kFirstName)
+                            UserDefaults.standard.set(lastName, forKey: kLastName)
+                            UserDefaults.standard.set(email, forKey: kEmail)
+                            UserDefaults.standard.set(true, forKey: kIsLoggedIn)
+                            firstName = ""
+                            lastName = ""
+                            email = ""
+                            isLoggedIn = true
+                        }
+                    }
+                    .buttonStyle(LittleLemonButtonStyle())
+                    
+                    Spacer()
                 }
-                .textFieldStyle(.roundedBorder)
-                .padding()
-                
-                if errorMessageShow {
-                    withAnimation() {
-                        Text(errorMessage)
-                            .foregroundColor(.red)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading)
+                .offset(y: contentOffset.height)
+                .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { notification in
+                    withAnimation {
+                        let keyboardRect = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+                        let keyboardHeight = keyboardRect.height
+                        self.isKeyboardVisible = true
+                        self.contentOffset = CGSize(width: 0, height: -keyboardHeight / 2 + 50)
+                    }
+                    
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { notification in
+                    withAnimation {
+                        self.isKeyboardVisible = false
+                        self.contentOffset = .zero
                     }
                 }
-                
-                Button("Register") {
-                    if validateUserInput(firstName: firstName, lastName: lastName, email: email) {
-                        UserDefaults.standard.set(firstName, forKey: kFirstName)
-                        UserDefaults.standard.set(lastName, forKey: kLastName)
-                        UserDefaults.standard.set(email, forKey: kEmail)
-                    }
-                }
-                .buttonStyle(LittleLemonButtonStyle())
-                
-                Spacer()
             }
-            .offset(y: contentOffset.height)
-            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { notification in
-                withAnimation {
-                    let keyboardRect = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
-                    let keyboardHeight = keyboardRect.height
-                    self.isKeyboardVisible = true
-                    self.contentOffset = CGSize(width: 0, height: -keyboardHeight / 2 + 50)
-                }
-                
-            }
-            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { notification in
-                withAnimation {
-                    self.isKeyboardVisible = false
-                    self.contentOffset = .zero
+            .onAppear() {
+                if UserDefaults.standard.bool(forKey: kIsLoggedIn) {
+                    isLoggedIn = true
                 }
             }
         }
