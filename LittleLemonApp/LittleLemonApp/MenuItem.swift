fb096275b7c2a9aa7bee2c964d0f9155060cac14
Jevgeni Vakker <bandem@icloud.com>
2023-03-22 22:43:07 +0100

    Fetching and storing the food menu

--- LittleLemonApp/LittleLemonApp/MenuItem.swift
+++ LittleLemonApp/LittleLemonApp/MenuItem.swift
@@ -8,10 +8,19 @@
 import Foundation
 
 
-struct MenuItem: Hashable, Identifiable {
+struct MenuItem: Codable, Identifiable {
     var id = UUID()
-    let nameItem : String
-    let descriptionItem: String
-    let priceItem: String
-    let pictureItem: String
+    let title : String
+    let descriptionDish: String
+    let price: String
+    let image: String
+    let category: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case descriptionDish = "description"
+        case price = "price"
+        case image = "image"
+        case category = "category"
+    }
 }
--- LittleLemonApp/LittleLemonApp/Views/FoodItem.swift
+++ LittleLemonApp/LittleLemonApp/Views/FoodItem.swift
@@ -7,35 +7,43 @@
 
 import SwiftUI
 
-struct FoodItem: View, Hashable {
+struct FoodItem: View {
     
-    let menuItem: MenuItem
+    @ObservedObject private var dish:Dish
+    
+    init(_ dish:Dish) {
+        self.dish = dish
+    }
     
     var body: some View {
         HStack {
             VStack {
-                Text(menuItem.nameItem)
+                Text(dish.title ?? "")
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .font(.sectionCategories())
                     .foregroundColor(.black)
                 Spacer(minLength: 10)
-                Text(menuItem.descriptionItem)
+                Text(dish.descriptionDish ?? "")
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .font(.paragraphText())
                     .foregroundColor(.primaryColor1)
                     .lineLimit(2)
                 Spacer(minLength: 5)
-                Text(menuItem.priceItem)
+                Text("$" + (dish.price ?? ""))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .font(.highlightText())
                     .foregroundColor(.primaryColor1)
                     .monospaced()
             }
-            Image(menuItem.pictureItem)
-                .resizable()
-                .aspectRatio( contentMode: .fill)
-                .frame(maxWidth: 90, maxHeight: 90)
-                .clipShape(Rectangle())
+            AsyncImage(url: URL(string: dish.image ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(maxWidth: 90, maxHeight: 90)
+            .clipShape(Rectangle())
         }
         .padding(.vertical)
         .frame(maxHeight: 150)
@@ -43,11 +51,17 @@ struct FoodItem: View, Hashable {
 }
 
 struct FoodItem_Previews: PreviewProvider {
+    static let context = PersistenceController.shared.container.viewContext
+    let dish = Dish(context: context)
     static var previews: some View {
-        
-        FoodItem(menuItem: MenuItem(nameItem: "Greek Salad",
-                                    descriptionItem: "The famous greek salad of crispy lettuce, peppers, olives and our chic...",
-                                    priceItem: "$12.99",
-                                    pictureItem: "greek-salad"))
+        FoodItem(oneDish())
+    }
+    static func oneDish() -> Dish {
+        let dish = Dish(context: context)
+        dish.title = "Hummus"
+        dish.descriptionDish = "Test"
+        dish.price = "10"
+        dish.image = "https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/greekSalad.jpg?raw=true"
+        return dish
     }
 }
